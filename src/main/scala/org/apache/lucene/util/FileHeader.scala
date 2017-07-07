a7007f80872a8d4ed11e94040e47f92fac120e15
bojdell <bojdell@gmail.com>
2017-07-06 21:50:29 -0700

    WIP - attempt at serializable file headers

--- src/main/scala/org/apache/lucene/util/FileHeader.scala
+++ src/main/scala/org/apache/lucene/util/FileHeader.scala
@@ -7,22 +7,63 @@
 
 package org.apache.lucene.util
 
-import java.io.RandomAccessFile
+import java.io._
 
 /**
   * Interface for writing unencrypted metadata at the beginning of an encrypted file.
   */
-abstract class FileHeader(raf: RandomAccessFile) {
-  // scalastyle:off null
-  var keyIdBytes: Array[Byte] = null
-  // scalastyle:on null
+abstract class FileHeader[T: Serializable](raf: RandomAccessFile) {
+
+  def headerContent: T
 
   /**
     * Writes the file header.
     */
-  def writeHeader(): Long
+  def writeHeader(): Long = {
+    writeByteArray(serialize(headerContent))
+
+    // return the current file pointer (i.e. header offset)
+    raf.getFilePointer
+  }
   /**
     * Reads the file header.
     */
-  def readHeader(): Unit
+  def readHeader(): T = {
+    deserialize(readBytesFromCurrentFilePointer())
+  }
+
+  protected def serialize(value: T): Array[Byte] = {
+    val stream: ByteArrayOutputStream = new ByteArrayOutputStream()
+    val oos = new ObjectOutputStream(stream)
+    oos.writeObject(value)
+    oos.close
+    stream.toByteArray
+  }
+
+  protected def deserialize(bytes: Array[Byte]): T = {
+    val ois = new ObjectInputStream(new ByteArrayInputStream(bytes))
+    val value = ois.readObject.asInstanceOf[T]
+    ois.close
+    value
+  }
+
+  /**
+    * Writes the byte array.
+    */
+  protected def writeByteArray(byteArray: Array[Byte]) {
+    raf.writeInt(byteArray.length)
+    raf.write(byteArray)
+  }
+
+  /**
+    * Read current bytes.
+    */
+  @throws[java.io.IOException]
+  protected def readBytesFromCurrentFilePointer(): Array[Byte] = {
+    /* Read the length of the following byte array in the file.*/
+    val num_bytes = raf.readInt
+    val byteArray = new Array[Byte](num_bytes)
+    raf.readFully(byteArray)
+    byteArray
+  }
 }
--- src/main/scala/org/apache/lucene/util/HmacFileHeader.scala
+++ src/main/scala/org/apache/lucene/util/HmacFileHeader.scala
@@ -7,71 +7,25 @@
 
 package org.apache.lucene.util
 
-import java.io.RandomAccessFile
+import java.io._
 
 import com.workday.elasticrypt.KeyProvider
+
+case class HmacHeaderContents(
+  keyId: String,
+  plainTextBytes: Array[Byte],
+  hmacBytes: Array[Byte]
+)
 
 /**
   * Implementation of the FileHeader interface that adds a MAC hash that is
   * used to verify that the correct key is being used to decrypt a file.
   */
-class HmacFileHeader(raf: RandomAccessFile, keyProvider: KeyProvider, keyId: String) extends FileHeader(raf) {
-
-  // scalastyle:off null
-  private var hmacBytes: Array[Byte] = null
-  private var plainTextBytes: Array[Byte] = null
-  // scalastyle:on null
-
-  /**
-    * Writes the file header.
-    */
-  def writeHeader(): Long = {
-    // Write keyId
-    keyIdBytes = keyId.getBytes
-    writeByteArray(keyIdBytes)
-
-    // Write plaintext bytes
+class HmacFileHeader(raf: RandomAccessFile, keyProvider: KeyProvider, keyId: String) extends FileHeader[HmacHeaderContents](raf) {
+  override def headerContent = {
     val numBytes = 8
-    plainTextBytes = HmacUtil.generateRandomBytes(numBytes)
-    writeByteArray(plainTextBytes)
-
-    // Write HMAC bytes
-    hmacBytes = HmacUtil.hmacValue(plainTextBytes, keyProvider.getKey(keyId))
-    writeByteArray(hmacBytes)
-
-    // return the current file pointer (i.e. header offset)
-    raf.getFilePointer
+    val plainTextBytes = HmacUtil.generateRandomBytes(numBytes)
+    val hmacBytes = HmacUtil.hmacValue(plainTextBytes, keyProvider.getKey(keyId))
+    HmacHeaderContents(keyId, plainTextBytes, hmacBytes)
   }
-
-  /**
-    * Writes the byte array.
-    */
-  private def writeByteArray(byteArray: Array[Byte]) {
-    raf.writeInt(byteArray.length)
-    raf.write(byteArray)
-  }
-
-  /**
-    * Reads the file header.
-    */
-  def readHeader(): Unit = {
-    raf.seek(0)
-
-    keyIdBytes = readBytesFromCurrentFilePointer
-    plainTextBytes = readBytesFromCurrentFilePointer
-    hmacBytes = readBytesFromCurrentFilePointer
-  }
-
-  /**
-    * Read current bytes.
-    */
-  @throws[java.io.IOException]
-  private def readBytesFromCurrentFilePointer: Array[Byte] = {
-    /* Read the length of the following byte array in the file.*/
-    val num_bytes: Int = raf.readInt
-    val byteArray: Array[Byte] = new Array[Byte](num_bytes)
-    raf.readFully(byteArray)
-    byteArray
-  }
-
 }
